38e96a6c3b1438d61fd34265172451b1b22a7960
Roman Koshchei <romankoshchei@gmail.com>
2023-07-21 20:47:09 +0300

    tailwind setup

--- apps/site/src/routes/+page.svelte
+++ apps/site/src/routes/+page.svelte
@@ -1,9 +1,3 @@
-<script lang="ts">
-  import type { PageData } from "./$types"
-
-  export let data: PageData
-</script>
-
 <!-- Landing page -->
 
 <svelte:head>
--- packages/ui/components/MyCounterButton.svelte
+++ packages/ui/components/MyCounterButton.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-  let count = 0;
+  let count = 0
 
   function handleClick() {
-    count += 1;
+    count += 1
   }
 </script>
 
-<button on:click="{handleClick}">
+<button class="bg-emerald-500 p-5" on:click={handleClick}>
   clicks: {count}
 </button>
