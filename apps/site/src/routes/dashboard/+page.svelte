70552f84723c0df5d5720602674893b57445c051
DmitryFlud2003 <valvech322@gmail.com>
2023-07-30 14:43:44 +0300

    added deleting

--- apps/site/src/routes/dashboard/+page.svelte
+++ apps/site/src/routes/dashboard/+page.svelte
@@ -36,11 +36,35 @@
       if(!shop) return alert("error of adding")
 
       shops.push(shop)
+      shops = shops
       return
     }
 
   }
 
+  async function deleteShop(shopId: string) {
+    const response = await Api.secureFetch(fetch, PUBLIC_API_URL, {
+     route: `/v1/dashboard/delete/${shopId}`,
+     method: "DELETE"
+    })
+
+    if (!response) throw error(500)
+
+    if (response.status == 403 || response.status == 401) throw redirect(302, "/login")
+
+    if (!response.ok) throw redirect(302, "/")
+
+    if (response.ok) {
+    
+      shops = shops.filter((x) => x.id != shopId)
+      return
+    }
+
+  }
+
+  async function toShop(shopId:string) {
+   goto(`/shop/${shopId}`)
+  }
 </script>
 
 <form
@@ -67,8 +91,29 @@
 
 <div id="list">
   <div>your shops:</div>
-{#each data.shops as shop}
-  <div>{shop.name}</div>
+<div class="flex flex-wrap justify-center mt-10">
+{#each shops as shop}
+<div class="max-w-sm p-6 m-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+  <h5 class="mb-3 text-base font-semibold text-gray-900 md:text-xl dark:text-white">
+    {shop.name}
+</h5>
+  <button
+  class="inline-block rounded bg-red-600 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white  transition duration-150 ease-in-out hover:bg-danger-600  focus:bg-danger-600  focus:outline-none focus:ring-0 active:bg-danger-700"
+  on:click={() => deleteShop(shop.id)}
+  type="submit"
+  >
+Delete
+</button>
+<button
+class="inline-block rounded bg-indigo-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white  transition duration-150 ease-in-out hover:bg-danger-600  focus:bg-danger-600  focus:outline-none focus:ring-0 active:bg-danger-700"
+  on:click={() => toShop(shop.id)}
+type="submit"
+>
+to Shop
+</button>
+</div>
+
 {/each}
+</div>
 </div>
 
